27658e1c57be8781ec3ca35712e4fef704f98a38
dkim127occ <dkim127@student.cccd.edu>
2016-09-15 14:03:25 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/dkim127/occars/Car.java
@@ -0,0 +1,64 @@
+package edu.orangecoastcollege.cs273.dkim127.occars;
+
+public class Car
+{
+    private double mDownPayment;
+    private double mPrice;
+    private int mLoanTerm;
+    private static final double TAX_RATE = 0.08;
+
+    public void calculateBorrowedAmount()
+    {
+
+    }
+
+    public void calculateInterestAmount()
+    {
+
+    }
+
+    public void calculateMonthlyPayment()
+    {
+
+    }
+
+    public void calculateTaxAmount()
+    {
+
+    }
+
+    public void calculateTotalCost()
+    {
+
+    }
+
+    public void setDownPayment(double downPayment)
+    {
+        mDownPayment = downPayment;
+    }
+
+    public void setLoanTerm(int loanTerm)
+    {
+        mLoanTerm = loanTerm;
+    }
+
+    public void setPrice(double price)
+    {
+        mPrice = price;
+    }
+
+    public double getDownPayment()
+    {
+        return mDownPayment;
+    }
+
+    public double getPrice()
+    {
+        return mPrice;
+    }
+
+    public int getLoanTerm()
+    {
+        return mLoanTerm;
+    }
+}
